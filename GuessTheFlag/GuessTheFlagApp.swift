e5187b269373c62ccd0a3eeeb7975e7dc5e7c9b0
enikebraimoh <richardbraimoh@gmail.com>
2022-07-03 23:10:55 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Enike Braimoh on 03/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
